f8386de4b79ba70f26642dd1e3bbbeef459c89ed
Ayhan Krala <codewolf27@gmail.com>
2023-08-22 13:13:19 +0300

    JB2AT1-5 create car entity and all related tables to it

---
+++ src/main/java/com/advanceacademy/moonlighthotel/entities/car/Car.java
@@ -0,0 +1,37 @@
+package com.advanceacademy.moonlighthotel.entities.car;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
+@Entity
+@Table(name = "cars")
+public class Car {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id",nullable = false)
+    private Long id ;
+
+    @NotNull
+    @Column(name = "model",nullable = false)
+    private String model ;
+
+    @NotNull
+    @Column(name = "year",nullable = false)
+    private Integer year ;
+
+    @NotNull
+    @Column(name = "make",nullable = false)
+    private String make ;
+
+    @ManyToOne
+    @JoinColumn(name = "cars_categories",nullable = false)
+    private CarCategory carCategories ;
+
+    // private List<FileResources> resources
+}
---
+++ src/main/java/com/advanceacademy/moonlighthotel/entities/car/CarType.java
@@ -0,0 +1,7 @@
+package com.advanceacademy.moonlighthotel.entities.car;
+
+public enum CarType {
+    SEDAN,
+    VAN,
+    SPORT
+}
