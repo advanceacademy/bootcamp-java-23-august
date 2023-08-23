ddbbbb7492e26fd6995a0b6643f1f309a83a02e5
Stiliyanov <stilrt@mail.bg>
2023-08-24 01:08:26 +0300

    Updated constraints to follow the ones from User class except for phone regex (not working) and improved message field capacity.

--- src/main/java/com/advanceacademy/moonlighthotel/entities/ContactUsForm.java
+++ src/main/java/com/advanceacademy/moonlighthotel/entities/ContactUsForm.java
@@ -19,24 +19,26 @@ public class ContactUsForm {
     @Column(nullable = false)
     private Long id;
 
-    @Column(name = "user_name")
     @NotNull
-    @Size(min = 2, max = 50, message = "User name must be between {min} and {max} characters")
+    @Column(name = "user_name")
+    @Size(min = 2, max = 255, message = "User name must be between {min} and {max} characters")
     private String userName;
 
-
     @NotNull
-    @Email(regexp = "^[^ @]+@[^ @]+\\.[^ @]+$", message = "Invalid email address format")
     @Column(name = "user_email")
+    @Size(min = 5, max = 255, message = "Email length must be between {min} and {max} characters")
+    @Email(regexp = "^[^ @]+@[^ @]+\\.[^ @]+$", message = "Invalid email address format")
     private String userEmail;
 
     @NotNull
-    @Pattern(regexp = "^(?:00|\\+)[0-9\\s.-]{6,20}$", message = "Invalid phone number format")
-    @Column(name = "user_phone_number", length = 30)
+    @Column(name = "user_phone_number")
+    @Size(max = 15, message = "Phone number length must be at most {max} characters")
+    @Pattern(regexp = "^(?:00|\\+)[0-9\\s.-]{3,15}$", message = "Invalid phone number format")
     private String userPhone;
 
     @NotNull
     @NotBlank(message = "Message may not be blank")
+    @Column(columnDefinition="TEXT")
     private String userMessage;
 
 }
