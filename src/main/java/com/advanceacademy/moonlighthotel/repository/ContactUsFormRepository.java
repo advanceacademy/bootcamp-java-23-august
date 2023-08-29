c39f8f1a7c0249b589591704de649e8fa13f8400
Ayhan Krala <codewolf27@gmail.com>
2023-08-29 18:32:53 +0300

    JB2AT1-10 fixed repository for contact us form

--- src/main/java/com/advanceacademy/moonlighthotel/repository/ContactUsFormRepository.java
+++ src/main/java/com/advanceacademy/moonlighthotel/repository/ContactUsFormRepository.java
@@ -3,15 +3,22 @@ package com.advanceacademy.moonlighthotel.repository;
 import com.advanceacademy.moonlighthotel.entity.ContactUsForm;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
-
 @Repository
-public interface ContactUsFormRepository extends JpaRepository<ContactUsForm,Long> {
-    @Query(nativeQuery = true,value = "SELECT c FROM ContactUsForm c WHERE c.userEmail = ?1")
-      Optional<List<ContactUsForm>> findByUserEmail (String userEmail);
-
+public interface ContactUsFormRepository extends JpaRepository<ContactUsForm, Long> {
+    @Query(nativeQuery = true, value = "SELECT * FROM ContactUsForm c " +
+            "WHERE (:userEmail IS NULL OR c.userEmail = :userEmail) " +
+            "AND (:userName IS NULL OR c.userName = :userName) " +
+            "AND (:userPhone IS NULL OR c.userPhone = :userPhone) " +
+            "AND (:userMessage IS NULL OR c.userMessage = :userMessage)")
+   Optional<List<ContactUsForm>> findByUserAttributes(
+            @Param("userEmail") String userEmail,
+            @Param("userName") String userName,
+            @Param("userPhone") String userPhone,
+            @Param("userMessage") String userMessage);
 }
