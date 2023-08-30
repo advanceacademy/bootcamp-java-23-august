8b818cdb6a8c25642b429cc42c6ea8d0b466c755
akkrastevak <atanas.krastev@abv.bg>
2023-08-30 13:21:58 +0300

    JB2AT1-11 formatting parts of the code

--- src/main/java/com/advanceacademy/moonlighthotel/repository/user/UserRepository.java
+++ src/main/java/com/advanceacademy/moonlighthotel/repository/user/UserRepository.java
@@ -12,26 +12,26 @@ import java.util.Optional;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
-    //That query method will return a list with users from the database, searching by firstname.
-    List<User> findByFirstName(String firstName);
+    //That query method will return an optional list with users from the database searching by firstname, or will return an empty list if there is no matches.
+    Optional<List<User>> findByFirstName(String firstName);
 
-    //That query method will return a list with users from the database, searching by lastname.
-    List<User> findByLastName(String lastName);
+    //That query method will return an optional list with users from the database searching by lastname, or will return an empty list if there is no matches.
+    Optional<List<User>> findByLastName(String lastName);
 
-    //That query method will return a list with users from the database, searching by firstname and lastname.
-    List<User> findByFirstNameAndLastName(String firstName, String lastName);
+    //That query method will return an optional list with users from the database searching by firstname and lastname, or will return an empty list if there is no matches.
+    Optional<List<User>> findByFirstNameAndLastName(String firstName, String lastName);
 
     //That query method will return a user from the database if there is a match with phone number. Or will return an empty Optional if there is no matches.
     Optional<User> findByPhoneNumber(String phoneNumber);
 
-    //That query method will return a user from the database if there is a match with email. Or will return an empty Optional if there is no matches.
+    //That query method will return a user from the database if there is a match with email. Or will return an empty optional if there is no matches.
     Optional<User> findByEmail(String email);
 
-    //That query method will return a list with users from the database, searching by created date.
-    List<User> findByCreatedDate(LocalDate createdDate);
+    //That query method will return an optional list with users from the database searching by created date, or will return an empty list if there is no matches.
+    Optional<List<User>> findByCreatedDate(LocalDate createdDate);
 
-    //That query method will return a list with users from the database, searching by their role.
-    List<User> findByUserRole(UserRole userRole);
+    //That query method will return an optional list with users from the database searching by their role, or will return an empty list if there is no matches.
+    Optional<List<User>> findByUserRole(UserRole userRole);
 
     //Check whether a user with a given email exists in the database or not.
     boolean existsByEmail(String email);
