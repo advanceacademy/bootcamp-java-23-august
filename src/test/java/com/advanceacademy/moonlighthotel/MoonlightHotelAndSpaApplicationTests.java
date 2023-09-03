0b735475dc729ad4e78d7a6354b7f846eb9c34c1
Ayhan Krala <codewolf27@gmail.com>
2023-09-03 19:29:21 +0300

    JB2AT1-21 testing the table reservation

--- src/test/java/com/advanceacademy/moonlighthotel/MoonlightHotelAndSpaApplicationTests.java
+++ src/test/java/com/advanceacademy/moonlighthotel/MoonlightHotelAndSpaApplicationTests.java
@@ -1,15 +1,15 @@
 package com.advanceacademy.moonlighthotel;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
 
 @SpringBootTest
 class MoonlightHotelAndSpaApplicationTests {
 
 	@Test
+	@Disabled
 	void contextLoads() {
-
-    }
+	}
 
 }
