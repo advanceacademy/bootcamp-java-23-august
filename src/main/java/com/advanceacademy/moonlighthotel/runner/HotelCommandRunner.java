7d070e942d63be3947aca7aa502d6c765fda3813
Boris Iliev <borisiliev006@abv.bg>
2023-09-08 11:44:35 +0300

    assets for the hotel are done

---
+++ src/main/java/com/advanceacademy/moonlighthotel/runner/HotelCommandRunner.java
@@ -0,0 +1,203 @@
+package com.advanceacademy.moonlighthotel.runner;
+
+import com.advanceacademy.moonlighthotel.entity.hotel.Room;
+import com.advanceacademy.moonlighthotel.entity.hotel.RoomFacility;
+import com.advanceacademy.moonlighthotel.entity.hotel.RoomType;
+import com.advanceacademy.moonlighthotel.entity.hotel.RoomView;
+import com.advanceacademy.moonlighthotel.repository.hotel.RoomFacilityRepository;
+import com.advanceacademy.moonlighthotel.repository.hotel.RoomRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Component
+public class HotelCommandRunner implements CommandLineRunner {
+
+    @Autowired
+    RoomFacilityRepository roomFacilityRepository;
+
+    @Autowired
+    RoomRepository roomRepository;
+
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        //Standard rooms
+        //Sea Standard rooms
+        Room standardSeaRoom1 = Room.builder()
+              .roomNumber(12)
+              .roomType(RoomType.STANDART)
+              .roomView(RoomView.SEA)
+              .roomPrice(220.00)
+              .build();
+
+        roomRepository.save(standardSeaRoom1);
+
+        Room standardSeaRoom2 = Room.builder()
+              .roomNumber(13)
+              .roomType(RoomType.STANDART)
+              .roomView(RoomView.SEA)
+              .roomPrice(220.00)
+              .build();
+
+        roomRepository.save(standardSeaRoom2);
+
+
+        //Standard Pool rooms
+        Room standardPoolRoom1 = Room.builder()
+                .roomNumber(14)
+                .roomType(RoomType.STANDART)
+                .roomView(RoomView.POOL)
+                .roomPrice(220.00)
+                .build();
+
+        roomRepository.save(standardPoolRoom1);
+
+        Room standardPoolRoom2 = Room.builder()
+                .roomNumber(15)
+                .roomType(RoomType.STANDART)
+                .roomView(RoomView.POOL)
+                .roomPrice(220.00)
+                .build();
+
+        roomRepository.save(standardPoolRoom2);
+
+
+        //Standard Garden rooms
+        Room standardGardenRoom1 = Room.builder()
+                .roomNumber(16)
+                .roomType(RoomType.STANDART)
+                .roomView(RoomView.GARDEN)
+                .roomPrice(220.00)
+                .build();
+
+        roomRepository.save(standardGardenRoom1);
+
+        Room standardGardenRoom2 = Room.builder()
+                .roomNumber(17)
+                .roomType(RoomType.STANDART)
+                .roomView(RoomView.GARDEN)
+                .roomPrice(220.00)
+                .build();
+
+        roomRepository.save(standardGardenRoom2);
+
+        Room standardGardenRoom3 = Room.builder()
+                .roomNumber(18)
+                .roomType(RoomType.STANDART)
+                .roomView(RoomView.GARDEN)
+                .roomPrice(220.00)
+                .build();
+
+        roomRepository.save(standardGardenRoom3);
+
+        Room standardGardenRoom4 = Room.builder()
+                .roomNumber(19)
+                .roomType(RoomType.STANDART)
+                .roomView(RoomView.GARDEN)
+                .roomPrice(220.00)
+                .build();
+
+        roomRepository.save(standardGardenRoom4);
+
+
+
+        //Studios
+        //Sea Studios
+        Room seaStudio1 = Room.builder()
+                .roomNumber(22)
+                .roomType(RoomType.STUDIO)
+                .roomView(RoomView.SEA)
+                .roomPrice(320.00)
+                .build();
+
+        roomRepository.save(seaStudio1);
+
+        Room seaStudio2 = Room.builder()
+                .roomNumber(23)
+                .roomType(RoomType.STUDIO)
+                .roomView(RoomView.SEA)
+                .roomPrice(320.00)
+                .build();
+
+        roomRepository.save(seaStudio2);
+
+
+        //Pool Studios
+        Room poolStudio1 = Room.builder()
+                .roomNumber(24)
+                .roomType(RoomType.STUDIO)
+                .roomView(RoomView.POOL)
+                .roomPrice(320.00)
+                .build();
+
+        roomRepository.save(poolStudio1);
+
+        Room poolStudio2 = Room.builder()
+                .roomNumber(25)
+                .roomType(RoomType.STUDIO)
+                .roomView(RoomView.POOL)
+                .roomPrice(320.00)
+                .build();
+
+        roomRepository.save(poolStudio2);
+
+
+        //Garden Studios
+        Room gardenStudio1 = Room.builder()
+                .roomNumber(26)
+                .roomType(RoomType.STUDIO)
+                .roomView(RoomView.GARDEN)
+                .roomPrice(320.00)
+                .build();
+
+        roomRepository.save(gardenStudio1);
+
+        Room gardenStudio2 = Room.builder()
+                .roomNumber(27)
+                .roomType(RoomType.STUDIO)
+                .roomView(RoomView.GARDEN)
+                .roomPrice(320.00)
+                .build();
+
+        roomRepository.save(gardenStudio2);
+
+
+
+        //Apartments
+        //Sea Apartments
+        Room seaApartment1 = Room.builder()
+                .roomNumber(32)
+                .roomType(RoomType.APARTMENT)
+                .roomView(RoomView.SEA)
+                .roomPrice(520.00)
+                .build();
+
+        roomRepository.save(seaApartment1);
+
+        Room seaApartment2 = Room.builder()
+                .roomNumber(33)
+                .roomType(RoomType.APARTMENT)
+                .roomView(RoomView.SEA)
+                .roomPrice(520.00)
+                .build();
+
+        roomRepository.save(seaApartment2);
+
+
+        //Pool Apartment
+        Room poolApartment1 = Room.builder()
+                .roomNumber(34)
+                .roomType(RoomType.APARTMENT)
+                .roomView(RoomView.POOL)
+                .roomPrice(520.00)
+                .build();
+
+        roomRepository.save(poolApartment1);
+    }
+}
