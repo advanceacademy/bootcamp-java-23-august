98a3c887fc9fc90b79083ec115653cb4aa5a1481
Ayhan Krala <codewolf27@gmail.com>
2023-09-15 14:28:13 +0300

    JB2AT1-23 - Added logic to create an admin user with a hashed password if not found.

--- src/main/java/com/advanceacademy/moonlighthotel/runner/UserRoleCommandLineRunner.java
+++ src/main/java/com/advanceacademy/moonlighthotel/runner/UserRoleCommandLineRunner.java
@@ -5,17 +5,11 @@ import com.advanceacademy.moonlighthotel.repository.user.UserRepository;
 import com.advanceacademy.moonlighthotel.repository.user.UserRoleRepository;
 import com.advanceacademy.moonlighthotel.service.user.UserRoleService;
 import com.advanceacademy.moonlighthotel.service.user.UserService;
-import lombok.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Component;
 import java.util.Optional;
-@Getter
-@Setter
-@Builder
-@AllArgsConstructor
-@NoArgsConstructor
 
 @Component
 public class UserRoleCommandLineRunner implements CommandLineRunner {
@@ -24,17 +18,28 @@ public class UserRoleCommandLineRunner implements CommandLineRunner {
     private static final String USER_ROLE_NAME = "ROLE_USER";
     private static final String ADMIN_EMAIL = "admin@example.com";
 
-    @Autowired
-    private UserService userService;
-    @Autowired
-    private UserRoleService userRoleService;
-    @Autowired
-    private UserRepository userRepository;
 
+    private final UserService userService;
+    private final UserRoleService userRoleService;
+    private final UserRepository userRepository;
+    private final UserRoleRepository userRoleRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+
+    //I've used constructor injection for dependencies, making the code more readable and testable.
     @Autowired
-    private UserRoleRepository userRoleRepository;
-    @Autowired
-    private BCryptPasswordEncoder bCryptPasswordEncoder;
+    public UserRoleCommandLineRunner(
+            UserService userService,
+            UserRoleService userRoleService,
+            UserRepository userRepository,
+            UserRoleRepository userRoleRepository,
+            BCryptPasswordEncoder bCryptPasswordEncoder) {
+        this.userService = userService;
+        this.userRoleService = userRoleService;
+        this.userRepository = userRepository;
+        this.userRoleRepository = userRoleRepository;
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+    }
 
     @Override
     public void run(String... args) throws Exception {
@@ -58,7 +63,7 @@ public class UserRoleCommandLineRunner implements CommandLineRunner {
         }
 
       //  Check if admin user with email "admin@example.com" exists
-        Optional<User> foundUser = userRepository.findByEmail("admin@example.com");
+        Optional<User> foundUser = userRepository.findByEmail(ADMIN_EMAIL);
 
         if (foundUser.isEmpty()) {
             User adminUser = User.builder()
@@ -66,10 +71,11 @@ public class UserRoleCommandLineRunner implements CommandLineRunner {
                     .lastName("User")
                     .email(ADMIN_EMAIL)
                     .phoneNumber("1234567890")
-                    .password(bCryptPasswordEncoder.encode("Ad!min123"))
+                    .password(bCryptPasswordEncoder.encode(ADMIN_EMAIL))
                     .userRole(adminUserRole)
                     .build();
             userRepository.save(adminUser);
         }
+
     }
 }
