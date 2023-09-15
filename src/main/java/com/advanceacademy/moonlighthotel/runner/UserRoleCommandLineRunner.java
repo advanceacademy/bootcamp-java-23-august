0346d73ce8803893f63da501a269bf43deb173a8
Ayhan Krala <codewolf27@gmail.com>
2023-09-15 14:34:01 +0300

    JB2AT1-23 - Add getter and setter for the user

--- src/main/java/com/advanceacademy/moonlighthotel/runner/UserRoleCommandLineRunner.java
+++ src/main/java/com/advanceacademy/moonlighthotel/runner/UserRoleCommandLineRunner.java
@@ -5,12 +5,15 @@ import com.advanceacademy.moonlighthotel.repository.user.UserRepository;
 import com.advanceacademy.moonlighthotel.repository.user.UserRoleRepository;
 import com.advanceacademy.moonlighthotel.service.user.UserRoleService;
 import com.advanceacademy.moonlighthotel.service.user.UserService;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Component;
 import java.util.Optional;
-
+@Getter
+@Setter
 @Component
 public class UserRoleCommandLineRunner implements CommandLineRunner {
 
