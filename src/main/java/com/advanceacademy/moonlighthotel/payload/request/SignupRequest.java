eb00b1a8fb7c3e0e49d8c82f8e63fb8210b113d5
Boris Iliev <borisiliev006@abv.bg>
2023-09-16 12:01:50 +0300

    security

---
+++ src/main/java/com/advanceacademy/moonlighthotel/payload/request/SignupRequest.java
@@ -0,0 +1,33 @@
+package com.advanceacademy.moonlighthotel.payload.request;
+
+import com.advanceacademy.moonlighthotel.entity.user.UserRole;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+
+import java.util.Set;
+
+public class SignupRequest {
+
+    @NotBlank
+    @Size(min = 2, max = 255, message = "First name length must be between {min} and {max} characters")
+    private String firstName;
+
+    @NotBlank
+    @Size(min = 2, max = 255, message = "Last name length must be between {min} and {max} characters")
+    private String lastName;
+
+    @NotBlank
+    @Size(min = 5, max = 255, message = "Email must be between {min} and {max} characters")
+    @Email(regexp = "^[^ @]+@[^ @]+\\.[^ @]+$", message = "Email must be valid")
+    private String email;
+
+    private UserRole userRole;
+
+    @NotBlank
+    @Size(min = 8, max = 255, message = "Password must be between {min} and {max} characters")
+    @Pattern(regexp = "^.*(?=.{8,})(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=]).*$")
+    private String password;
+
+}
