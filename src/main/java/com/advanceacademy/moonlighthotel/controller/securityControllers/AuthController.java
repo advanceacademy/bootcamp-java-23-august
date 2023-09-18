f8925c3e089ff7961d7d30869c176f5b1cab1aa8
Boris Iliev <borisiliev006@abv.bg>
2023-09-18 18:24:22 +0300

    security almost done added controller

---
+++ src/main/java/com/advanceacademy/moonlighthotel/controller/securityControllers/AuthController.java
@@ -0,0 +1,91 @@
+package com.advanceacademy.moonlighthotel.controller.securityControllers;
+
+import com.advanceacademy.moonlighthotel.payload.request.LoginRequest;
+import com.advanceacademy.moonlighthotel.payload.request.SignupRequest;
+import com.advanceacademy.moonlighthotel.payload.response.UserInfoResponse;
+import com.advanceacademy.moonlighthotel.repository.user.UserRepository;
+import com.advanceacademy.moonlighthotel.repository.user.UserRoleRepository;
+import com.advanceacademy.moonlighthotel.security.jwt.JwtUtils;
+import com.advanceacademy.moonlighthotel.security.services.UserDetailsImpl;
+import com.advanceacademy.moonlighthotel.service.user.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseCookie;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@CrossOrigin(origins = "*", maxAge = 3600)
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+
+    @Autowired
+    AuthenticationManager authenticationManager;
+
+    @Autowired
+    UserService userService;
+
+    @Autowired
+    UserRoleRepository roleRepository;
+
+    @Autowired
+    PasswordEncoder encoder;
+
+    @Autowired
+    JwtUtils jwtUtils;
+
+    @Operation(summary = "Register user")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "User created",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = UserInfoResponse.class)) }),
+            @ApiResponse(responseCode = "409", description = "User with email already used",
+                    content = @Content) })
+    @PostMapping(path = "/register")
+    public ResponseEntity<UserInfoResponse> registerUser(@Valid @RequestBody SignupRequest request) {
+        UserInfoResponse response = userService.createNewUser(request);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+
+    @PostMapping("/signin")
+    public ResponseEntity<?> authenticateUser(@Valid @RequestBody LoginRequest loginRequest) {
+
+        Authentication authentication = authenticationManager
+                .authenticate(new UsernamePasswordAuthenticationToken(loginRequest.getEmail(), loginRequest.getPassword()));
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        UserDetailsImpl userDetails = (UserDetailsImpl) authentication.getPrincipal();
+
+        ResponseCookie jwtCookie = jwtUtils.generateJwtCookie(userDetails);
+
+        List<String> roles = userDetails.getAuthorities().stream()
+                .map(item -> item.getAuthority())
+                .collect(Collectors.toList());
+
+        return ResponseEntity.ok().header(HttpHeaders.SET_COOKIE, jwtCookie.toString())
+                .body(new UserInfoResponse(userDetails.getId(),
+                        userDetails.getFirstName(),
+                        userDetails.getLastName(),
+                        userDetails.getEmail(),
+                        roles.toString()));
+    }
+
+
+}
