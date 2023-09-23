e32123220778798b36d5fc78e16e5e97e215e19b
Boris Iliev <borisiliev006@abv.bg>
2023-09-23 23:16:20 +0300

    security almost done controller 1.31

--- src/main/java/com/advanceacademy/moonlighthotel/service/user/impl/UserServiceImpl.java
+++ src/main/java/com/advanceacademy/moonlighthotel/service/user/impl/UserServiceImpl.java
@@ -1,24 +1,28 @@
 package com.advanceacademy.moonlighthotel.service.user.impl;
 
 import com.advanceacademy.moonlighthotel.entity.user.User;
+import com.advanceacademy.moonlighthotel.payload.request.LoginRequest;
 import com.advanceacademy.moonlighthotel.payload.request.SignupRequest;
+import com.advanceacademy.moonlighthotel.payload.response.JwtResponse;
 import com.advanceacademy.moonlighthotel.payload.response.UserInfoResponse;
 import com.advanceacademy.moonlighthotel.repository.user.UserRepository;
 import com.advanceacademy.moonlighthotel.service.user.UserService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
 @Service
+@RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
 
-    @Autowired
-    public UserServiceImpl(UserRepository userRepository) {
-        this.userRepository = userRepository;
-    }
+    private final AuthenticationManager manager;
 
     @Override
     public User createUser(User user) {
@@ -36,6 +40,7 @@ public class UserServiceImpl implements UserService {
 
         return newUser;
     }
+
 
     @Override
     public Optional<User> getUserById(Long id) {
