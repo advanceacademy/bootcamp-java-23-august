3f87b2fbe8424887809d1ae557ac5098a06db9d3
Boris Iliev <borisiliev006@abv.bg>
2023-09-26 21:34:38 +0300

    security major changes 1.34

--- src/main/java/com/advanceacademy/moonlighthotel/security/WebSecurityConfig.java
+++ src/main/java/com/advanceacademy/moonlighthotel/security/WebSecurityConfig.java
@@ -1,41 +1,36 @@
 package com.advanceacademy.moonlighthotel.security;
 
-import com.advanceacademy.moonlighthotel.runner.SecurityConfig;
-import com.advanceacademy.moonlighthotel.security.jwt.AuthEntryPointJwt;
 import com.advanceacademy.moonlighthotel.security.jwt.AuthTokenFilter;
-import com.advanceacademy.moonlighthotel.security.services.UserDetailsServiceImpl;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
-import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
-@EnableMethodSecurity
-(securedEnabled = true,
-jsr250Enabled = true,
-prePostEnabled = true)  // by default
+@RequiredArgsConstructor
 public class WebSecurityConfig {// extends WebSecurityConfigurerAdapter {
-  @Autowired
-  UserDetailsServiceImpl userDetailsService;
 
-  @Autowired
-  SecurityConfig securityConfig;
-
-  @Autowired
-  private AuthEntryPointJwt unauthorizedHandler;
+  private final AuthTokenFilter authTokenFilter;
+  private final AuthenticationProvider authenticationProvider;
 
   @Bean
-  public AuthTokenFilter authenticationJwtTokenFilter() {
-    return new AuthTokenFilter();
+  public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    http.csrf(AbstractHttpConfigurer::disable)
+        .authorizeHttpRequests(auth ->
+          auth.requestMatchers("/api/auth/**").permitAll()
+              .anyRequest().authenticated())
+            .sessionManagement(sessionManagementConfig -> sessionManagementConfig.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+            .authenticationProvider(authenticationProvider)
+            .addFilterBefore(authTokenFilter, UsernamePasswordAuthenticationFilter.class);
+
+    return http.build();
   }
 
 //  @Override
@@ -43,15 +38,15 @@ public class WebSecurityConfig {// extends WebSecurityConfigurerAdapter {
 //    authenticationManagerBuilder.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
 //  }
 
-  @Bean
-  public DaoAuthenticationProvider authenticationProvider() {
-      DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
-       
-      authProvider.setUserDetailsService(userDetailsService);
-      authProvider.setPasswordEncoder(securityConfig.bCryptPasswordEncoder());
-   
-      return authProvider;
-  }
+  //@Bean
+  //public DaoAuthenticationProvider authenticationProvider() {
+  //    DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+  //
+  //    authProvider.setUserDetailsService(userDetailsService);
+  //    authProvider.setPasswordEncoder(securityConfig.bCryptPasswordEncoder());
+  //
+  //    return authProvider;
+  //}
   
 //  @Bean
 //  @Override
@@ -59,10 +54,10 @@ public class WebSecurityConfig {// extends WebSecurityConfigurerAdapter {
 //    return super.authenticationManagerBean();
 //  }
   
-  @Bean
-  public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception {
-    return authConfig.getAuthenticationManager();
-  }
+  //@Bean
+  //public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception {
+  //  return authConfig.getAuthenticationManager();
+  //}
 
 // @Bean
 // public PasswordEncoder passwordEncoder() {
@@ -81,21 +76,5 @@ public class WebSecurityConfig {// extends WebSecurityConfigurerAdapter {
 //    http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
 //  }
   
-  @Bean
-  public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-    http.csrf(csrf -> csrf.disable())
-        .exceptionHandling(exception -> exception.authenticationEntryPoint(unauthorizedHandler))
-        .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-        .authorizeHttpRequests(auth -> 
-          auth.requestMatchers("/api/auth/**").permitAll()
-              .requestMatchers("/api/test/**").permitAll()
-              .anyRequest().authenticated()
-        );
-    
-    http.authenticationProvider(authenticationProvider());
 
-    http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
-    
-    return http.build();
-  }
 }
