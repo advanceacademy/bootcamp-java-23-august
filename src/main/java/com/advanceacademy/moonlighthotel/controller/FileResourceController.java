6e80baf31b8d20021270e79b4cbcc7cf11345d34
akkrastevak <atanas.krastev@abv.bg>
2023-09-27 14:24:24 +0300

    JB2AT1-33 get car photos (format code)

--- src/main/java/com/advanceacademy/moonlighthotel/controller/FileResourceController.java
+++ src/main/java/com/advanceacademy/moonlighthotel/controller/FileResourceController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping(value = "/resources")
+@RequestMapping(value = "/api/v1/resources")
 public class FileResourceController {
 
     private final FileResourceService fileResourceService;
