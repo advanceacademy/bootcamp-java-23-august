098463cdd96b4afe5696a38c01a9a0dbdf6d181a
akkrastevak <atanas.krastev@abv.bg>
2023-10-17 20:20:51 +0300

    JB2AT1-31 Get Screen Events v1(need to rework the filterchain)

--- src/main/java/com/advanceacademy/moonlighthotel/controller/barZone/ScreenEventController.java
+++ src/main/java/com/advanceacademy/moonlighthotel/controller/barZone/ScreenEventController.java
@@ -1,6 +1,8 @@
 package com.advanceacademy.moonlighthotel.controller.barZone;
 
+import com.advanceacademy.moonlighthotel.converter.barZone.ScreenEventConverter;
 import com.advanceacademy.moonlighthotel.dto.barZone.ScreenEventDTO;
+import com.advanceacademy.moonlighthotel.dto.barZone.ScreenEventResponse;
 import com.advanceacademy.moonlighthotel.entity.barZone.ScreenEvent;
 import com.advanceacademy.moonlighthotel.exception.DuplicateRecordException;
 import com.advanceacademy.moonlighthotel.service.barZone.ScreenEventService;
@@ -8,21 +10,22 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/screen-events")
 public class ScreenEventController {
     private final ScreenEventService screenEventService;
+    private final ScreenEventConverter screenEventConverter;
 
     @Autowired
-    public ScreenEventController(ScreenEventService screenEventService) {
+    public ScreenEventController(ScreenEventService screenEventService, ScreenEventConverter screenEventConverter) {
         this.screenEventService = screenEventService;
+        this.screenEventConverter = screenEventConverter;
     }
 
     @PostMapping("/create")
@@ -42,5 +45,33 @@ public class ScreenEventController {
         } catch (DuplicateRecordException e) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
         }
+    }
+
+    @GetMapping("/by-date/{eventDate}")
+    public ResponseEntity<List<ScreenEventResponse>> getAllScreenEventsByDate (@PathVariable LocalDate eventDate){
+        List<ScreenEvent> foundEvents = screenEventService.getByEventsDate(eventDate);
+
+        if (foundEvents.isEmpty()){
+            return ResponseEntity.notFound().build();
+        }
+
+        List<ScreenEventResponse> screenEventResponses = foundEvents.stream()
+                .map(screenEventConverter::response)
+                .collect(Collectors.toList());
+        return ResponseEntity.ok(screenEventResponses);
+    }
+
+    @GetMapping("/by-name/{eventName}")
+    public ResponseEntity<ScreenEventResponse> getScreenEventByName (@PathVariable String eventName){
+        ScreenEvent foundEvent = screenEventService.getEventByName(eventName);
+        ScreenEventResponse response = screenEventConverter.response(foundEvent);
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/by-id/{requestId}")
+    public ResponseEntity<ScreenEventResponse> getScreenEventById (@PathVariable Long requestId){
+        ScreenEvent foundEvent = screenEventService.getScreenEventById(requestId);
+        ScreenEventResponse response = screenEventConverter.response(foundEvent);
+        return ResponseEntity.ok(response);
     }
 }
