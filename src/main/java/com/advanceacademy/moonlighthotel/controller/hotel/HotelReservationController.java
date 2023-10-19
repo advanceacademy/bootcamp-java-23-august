9bc9699d79ee186e25523235923be2e401cde476
Ayhan Krala <codewolf27@gmail.com>
2023-10-19 14:41:11 +0300

    JB2AT1-39- create the reservation controller

---
+++ src/main/java/com/advanceacademy/moonlighthotel/controller/hotel/HotelReservationController.java
@@ -0,0 +1,10 @@
+package com.advanceacademy.moonlighthotel.controller.hotel;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/hotel-reservations")
+public class HotelReservationController {
+
+}
