99966655d762ae989d0c3bb4f3fecf4627bcdbdf
akkrastevak <atanas.krastev@abv.bg>
2023-10-20 12:21:48 +0300

    JB2AT1-43 resolved conflicts

--- src/main/java/com/advanceacademy/moonlighthotel/service/user/UserService.java
+++ src/main/java/com/advanceacademy/moonlighthotel/service/user/UserService.java
@@ -33,4 +33,6 @@ public interface UserService {
 
     void deleteUser(Long userId);
 
+    List<User> searchUsers(Long id, String email, String firstName, String lastName, String phoneNumber);
+
 }
